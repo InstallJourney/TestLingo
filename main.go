75f9197da6362c232e4cf5a4fdd4db17a3aca1f0
InstallJourney <CodeLingoUser@gmail.com>
2019-12-03 15:32:19 +1300

    Initial Commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	f, err := os.Create("test.txt")
+	if err != nil {
+		fmt.Println("Failed to create file")
+	}
+
+	_, err = f.WriteString("Some words")
+	if err != nil {
+		fmt.Println("Failed to write to file")
+	}
+}
